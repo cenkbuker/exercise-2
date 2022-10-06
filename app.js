1a32a857bb8eb88a15f2df841ae8e33d5331a0dd
Cenk Buker <cenk@Cenks-MacBook-Pro.local>
2022-10-06 00:52:24 -0500

    for-of changed

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-let first = document.getElementById('container')
-let second = document.querySelector('#container')
-let third = document.querySelectorAll('.second')
-let fourth = document.querySelector('ol .third')
-let fifth = document.querySelector('#container')
-fifth.innerHTML = 'Hello!'
-let sixth = document.querySelector('.footer')
-sixth.classList.add('main')
-sixth.classList.remove('main')
-let eighth = document.createElement('li')
-eighth.innerHTML=('four')
-let tenth = document.querySelector('ul')
-tenth.appendChild(eighth)
+// let first = document.getElementById('container')
+// let second = document.querySelector('#container')
+// let third = document.querySelectorAll('.second')
+// let fourth = document.querySelector('ol .third')
+// let fifth = document.querySelector('#container')
+// fifth.innerHTML = 'Hello!'
+// let sixth = document.querySelector('.footer')
+// sixth.classList.add('main')
+// sixth.classList.remove('main')
+// let eighth = document.createElement('li')
+// eighth.innerHTML=('four')
+// let tenth = document.querySelector('ul')
+// tenth.appendChild(eighth)
 let eleventh = document.querySelectorAll('ol li')
-for(let i = 0; i < eleventh.length; i++){
-    eleventh[i].style.backgroundColor = "green";
+for(let i of eleventh){
+    i.style.backgroundColor = "green";
 }
 let twelveth = document.querySelector('.footer')
 twelveth.remove()
